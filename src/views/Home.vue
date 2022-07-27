f91d93853c14f018821ae9781e4e8d7979aa5b5c
Roxana <ms.roxanarahimi@gmail.com>
2022-07-27 12:28:28 +0430

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="home">
+    <button class="btn btn-primary p-5">rox btn</button>
+    <p class="bg-dark text-primary ">
+      rox p
+    </p>
+    <div>
+      <input type="checkbox" class="form-check-input" checked name="ss" id="">
+    </div>
+    <div class="form-check form-switch">
+      <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault">
+      <label class="form-check-label" for="flexSwitchCheckDefault">Default switch checkbox input</label>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Home',
+  components: {
+
+  }
+}
+</script>
---
+++ src/views/Login.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="login">
+    <div class="row">
+      <div class="col-6 vh-100">
+        <div class="row justify-content-center">
+          <div class="col-xl-5">
+            <form action="" style="margin-top: 30vh">
+              <p class="fw-bold mb-5" style="font-size: 22px">ورود به <span class="text-primary"> فارس گیمر</span></p>
+
+              <div class="col-xl-12 mb-4">
+              <label for="mobile" class="small text-muted mb-4" style="font-size: 12px">شماره همراه</label>
+              <input id="mobile" class="form-control mb-5 en" type="text">
+              </div>
+              <div class="col-xl-12 mb-4">
+                <button class="btn btn-primary py-2 px-5" type="submit">
+                  <p class="my-1">تایید</p>
+                </button>
+              </div>
+              <p class="small"  style="font-size: 16px">حساب کاربری ندارید؟ <a class="text-primary">ثبت نام</a></p>
+
+            </form>
+          </div>
+        </div>
+      </div>
+      <div class="col-6 bg-primary vh-100"></div>
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Login"
+}
+</script>
+
+<style scoped>
+button{
+  height: 52px;
+  width: 140px;
+  border-radius: 8px;
+  background-image: conic-gradient(from 30deg, #7007FA 10%, #9d56ff 120%, #7007FA 100%);
+
+}
+input{
+  height: 50px;
+  border-radius: 8px;
+
+}
+</style>
