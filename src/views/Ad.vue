18b100f79855511c2b1bed5eabe5a0c0cc66a911
Roxana <ms.roxanarahimi@gmail.com>
2022-09-15 22:38:52 +0430

    mm

--- src/views/Ad.vue
+++ src/views/Ad.vue
@@ -36,9 +36,9 @@
                 <i class="bi bi-chevron-right " ></i>
               </div>
               <img src="/img/sample.jpg" style="width: 100%; height: 100%" alt="">
-              <div class="bg-light" style=" position: absolute;top: 200px; left: 10px;
+              <div class="bg-light text-center" style=" position: absolute;top: 200px; left: 10px;
                font-size: 25px; width: 30px;height: 30px ;border-radius: 50%">
-                <i class="bi bi-chevron-left " ></i>
+                <i class="bi bi-chevron-left" ></i>
               </div>
             </div>
           </div>
@@ -46,9 +46,9 @@
         <div class="col-2"></div>
         <div class="col-10">
           <div class="card bg-light">
-            <h3>
+            <h4 class="py-3 px-2">
               گزارش تخلف
-            </h3>
+            </h4>
           </div>
         </div>
       </div>
@@ -68,18 +68,73 @@
         </div>
 
       </div>
-        <p class="fw-bold mb-0 px-3"> اکانت فورتناین از سیزن دو</p>
-        <small class="px-3"> اکانت فورتناین از سیزن دو</small>
+        <p class="fw-bold mb-0 px-3 mt-3"> اکانت فورتناین از سیزن دو</p>
+        <small class="px-3 mb-3"> اکانت فورتناین از سیزن دو</small>
+      </div>
+
+      <div class="text-start mt-4">
+
+
+        <a class="text-primary" >چت آنلاین</a>
+        <button class="btn btn-outline-primary  me-4" style="height: 42px" >اطلاعات تماس</button>
+        <btn-primary-shadow  class=" me-2">معامله آنلاین</btn-primary-shadow>
+
+      </div>
+      <div>
+        <h4 class="mt-4">مشخصات</h4>
+        <table class=" w-100 ">
+          <tbody>
+          <tr class="border-bottom">
+            <td class="border-start ">بتل پس :1تا5 مدرن وارفر 2تا4  کلدوار</td>
+            <td class="pe-4">لول کوین: 85</td>
+          </tr>
+          <tr class=" border-bottom">
+            <td class="border-start">لول کوین: 85</td>
+            <td class="pe-4">لول کوین: 85</td>
+          </tr>
+          <tr  class="border-bottom">
+            <td class="border-start">لول کوین: 85</td>
+            <td class="pe-4">لول کوین: 85</td>
+          </tr>
+          </tbody>
+        </table>
+      </div>
+      <div>
+        <h4 class="mt-4">توضیحات</h4>
+        کانت فورتناین از سیزن دو
+        اکانت فورتناین از سیزن دوکانت فورتناین از سیزن دو
+        اکانت فورتناین از سیزن دوکانت فورتناین از سیزن دو
+        اکانت فورتناین از سیزن دوکانت فورتناین از سیزن دو
+        اکانت فورتناین از سیزن دوکانت فورتناین از سیزن دو
+        اکانت فورتناین از سیزن دوکانت فورتناین از سیزن دو
+        اکانت فورتناین از سیزن دوکانت فورتناین از سیزن دو
+        اکانت فورتناین از سیزن دوکانت فورتناین از سیزن دو
+        اکانت فورتناین از سیزن دوکانت فورتناین از سیزن دو
+        اکانت فورتناین از سیزن دوکانت فورتناین از سیزن دو
+        اکانت فورتناین از سیزن دوکانت فورتناین از سیزن دو
+        اکانت فورتناین از سیزن دوکانت فورتناین از سیزن دو
+        اکانت فورتناین از سیزن دوکانت فورتناین از سیزن دو
+        اکانت فورتناین از سیزن دوکانت فورتناین از سیزن دو
+        اکانت فورتناین از سیزن دوکانت فورتناین از سیزن دو
+        اکانت فورتناین از سیزن دوکانت فورتناین از سیزن دو
+        اکانت فورتناین از سیزن دوکانت فورتناین از سیزن د
+        اکانت فورتناین از سیزن دوکانت فورتناین از سیزن د
+        اکانت فورتناین از سیزن دوکانت فورتناین از سیزن دو
+        اکانت فورتناین از سیزن دوکانت فورتناین از سیزن دو
+        اکانت فورتناین از سیزن دوکانت فورتناین از سیزن دو
+        اکانت فورتناین از سیزن دو
       </div>
     </div>
+
   </div>
 </template>
 
 <script>
 import ClientSideBar from "@/components/ClientSideBar";
+import BtnPrimaryShadow from "@/components/BtnPrimaryShadow";
 export default {
   name: "Ad",
-  components: {ClientSideBar}
+  components: {BtnPrimaryShadow, ClientSideBar}
 }
 </script>
 
